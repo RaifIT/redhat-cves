c6b02dd44a769a9b52fafd5ededc8539487527a8
Raif IT <raif5it@gmail.com>
2021-05-26 16:06:52 +0300

    Added aggregating dict to highlight dates with number of CVEs

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ import csv
 
 base_url = "https://access.redhat.com/hydra/rest/securitydata"
 
+
 '''Query API with specified parameters
 Returns JSON http response'''
 def query_api(params):
@@ -17,11 +18,13 @@ Returns dict of the JSON response'''
 def get_cves_between_dict(start_date, end_date):
     return json.loads(get_cves_between(start_date, end_date))
 
+
 '''Get the CVEs in a specified date range from start_date to end_date as a JSON
 Returns content of the JSON response'''
 def get_cves_between(start_date, end_date):
     response = query_api({'after': start_date, 'before': end_date})
     return response.content
+
 
 '''Write a python dictionary to a CSV file named cves.csv'''
 def write_dict_list_to_csv(dict_list):
@@ -30,6 +33,7 @@ def write_dict_list_to_csv(dict_list):
         dict_writer = csv.DictWriter(output_file, keys)
         dict_writer.writeheader()
         dict_writer.writerows(dict_list)
+
 
 '''Convert a regular API response dict into a dict that groups public_date keys with a list of CVEs
 Returns a dictionary of {public_date: [{bugzilla_description, CVE}, {},...]}'''
@@ -43,10 +47,12 @@ def dict_list_to_timed_dict_list(dict_list):
             timed_dict_list[dict['public_date']] = [entry]
     return timed_dict_list
 
+
 '''Returns the number of CVEs in a single entry of a timed_dict. i.e. the number of CVEs on a specific date'''
 def count_cves(timed_dict_entry):
     cves = get_cves(timed_dict_entry)
     return len(cves)
+
 
 '''Returns a set of CVEs a single entry of a timed_dict. i.e. the unique CVEs on a specific date'''
 def get_cves(timed_dict_entry):
@@ -54,6 +60,21 @@ def get_cves(timed_dict_entry):
     for dict in timed_dict_entry:
         cves.add(dict['CVE'])
     return cves
+
+
+'''Aggregate the results in a timed_dict by date
+Returns a list of dictionaries of {public_date, cve_count}'''
+def aggregate_timed_dict(timed_dict):
+    aggregated_dict = []
+    for public_date, dict in timed_dict.items():
+        aggregated_dict.append({'public_date': public_date, 'cve_count': count_cves(dict)})
+    return aggregated_dict
+
+
+'''Write the list of {public_date, cve_count} to a CSV file named aggregated.csv'''
+def aggregated_to_csv(aggregated_dict):
+    with open('cves.csv', 'w', newline='')  as output_file:
+        csv
 
 
 def test():
@@ -80,6 +101,9 @@ def test():
     print(timed_dict['2021-05-25T00:00:00Z'])
     print(get_cves(timed_dict['2021-05-25T00:00:00Z']))
     print(count_cves(timed_dict['2021-05-25T00:00:00Z']))
-
+    print(aggregate_timed_dict(timed_dict))
+    print(aggregate_timed_dict(timed_dict)[0])
+    print(aggregate_timed_dict(timed_dict)[0].keys())
+    #write_dict_list_to_csv(aggregate_timed_dict(timed_dict))
 
 test()
